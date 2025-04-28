0fb64bf2dd85d8a3bd339e22696f59aaa5d34e77
Robson Alves <contato@robsonalves.dev.br>
2025-04-28 10:17:15 -0300

    refactor: restructure route handling for improved middleware usage

--- packages/ntask/routes/index.mjs
+++ packages/ntask/routes/index.mjs
@@ -1,8 +1,17 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.json({ status: "NTask API" });
-});
+router
+	.route("/")
+	.all((req, res, next) => {
+		console.log(`Path: ${req.path}`);
+		next();
+	})
+	.get((req, res) => {
+		res.json({ status: "NTask API" });
+	})
+	.post((req, res) => {
+		res.status(405).json({ error: "Method Not Allowed" });
+	});
 
 export default router;
