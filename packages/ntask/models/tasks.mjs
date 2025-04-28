6ac6f74d84e3ce7bd536945d3f46a0b53322697f
Robson Alves <contato@robsonalves.dev.br>
2025-04-28 16:29:18 -0300

    refactor: update Tasks model to use named export and adjust import in routes

--- packages/ntask/models/tasks.mjs
+++ packages/ntask/models/tasks.mjs
@@ -1,9 +1,14 @@
-// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
-class Tasks {
-	static findAll(params, callback) {
-		const tasks = [{ title: "Fazer compras" }, { title: "Consertar o pc" }];
-		callback(tasks);
-	}
-}
+// // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
+// class Tasks {
+// 	static findAll(params, callback) {
+// 		const tasks = [{ title: "Fazer compras" }, { title: "Consertar o pc" }];
+// 		callback(tasks);
+// 	}
+// }
 
-export default Tasks;
+// export default Tasks;
+
+export function findAll(params, callback) {
+	const tasks = [{ title: "Fazer compras" }, { title: "Consertar o pc" }];
+	callback(tasks);
+}
--- packages/ntask/routes/tasks.mjs
+++ packages/ntask/routes/tasks.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import Tasks from "../models/tasks.mjs";
+import * as Tasks from "../models/tasks.mjs";
 const router = express.Router();
 
 router.get("/tasks", (req, res) => {
